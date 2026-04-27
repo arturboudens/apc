18d9dab70a89bc457b705fd0531d1589dceb0c69
artur <arturbouden@gmail.com>
2026-04-27 13:41:52 -0300

    lista funções

---
+++ listaIO2/4.py
@@ -0,0 +1,9 @@
+num1 = input()
+num2 = input()
+
+print(f'I1 = {num1}, I2 = {num2}')
+print(f'I1 = {num1:10}, I2 = {num2}')
+print(f'I1 = {num1}, I2 = {num2:>5}')
+print(f'I1 = {num1:10}, I2 = {num2:<04}')
+print(f'I1 = {num1:06<}, I2 = {num2:<04}')
+
