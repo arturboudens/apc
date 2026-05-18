7930bee09ab8f4a6f09e2f4c7fddd0f6ed470e4a
artur <arturboudens@gmail.com>
2026-05-18 13:18:51 -0300

    Simulado P1

---
+++ simuladoP1/combo.py
@@ -0,0 +1,7 @@
+def hadouken():
+    return baixo(), diagonal_frente(), frente(), soco(), print("Hadouken !")
+def shoryuken():
+    return frente(), baixo(), diagonal_frente(), soco(), print("Shoryuken !")
+def tatsumaki_senpukyaku():
+    frase = "Tatsumaki Senpukyaku !"
+    return baixo(), diagonal_tras(), tras(), chute(), print(frase)
